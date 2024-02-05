2c5c3cb003b65d3bd2e4c13e03fdf4181f6a38d5
KolyanGrom <kolyangrom@yandex.ru>
2024-02-05 17:34:46 +0700

    Введение одного условного оператора if-else

--- src/Main.java
+++ src/Main.java
@@ -2,24 +2,16 @@ public class Main {
     public static void main(String[] args) {
 
         int score = 100;
-        int topup = 1000;
+        int topup = 1500;
         int amount = (score + topup);
         int bounse = (topup / 100 * 1);
 
 
         int balance = (score + topup + bounse);
         if (topup > 1000) {
-            System.out.println("Баланс счета при пополнении на " + topup + " руб." + "равен " + balance + " руб.");
-        }
-        if (topup < 1000) {
-            System.out.println("Баланс счета при пополнении на " + topup + " руб." + "равен " + amount + " руб.");
+            System.out.println("Баланс счета при пополнении на " + topup + " руб. " + "равен: " + balance + " руб.");
         } else {
-            balance = (score + topup);
+            System.out.println("Баланс счета при пополнении на " + topup + " руб." + "равен " + amount + " руб.");
         }
-        if (topup == 1000) {
-            System.out.println("Баланс счета при пополнении на 1000 руб" + "равен " + amount + " руб.");
-        }
-
-
     }
 }
